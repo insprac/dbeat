a9383a7975283b74802210826d7b2f44b1380753
insprac <insprac.dev@gmail.com>
2025-01-20 07:34:51 +1300

    added layout for all pages

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+    const { children } = $props();
+</script>
+
+<div class="layout-container">
+    <div class="sidebar">
+        <nav>
+            <a href="/mixes">Mixes</a>
+        </nav>
+    </div>
+
+    <main>
+        {@render children()}
+    </main>
+</div>
+
+<style>
+    .layout-container {
+        display: flex;
+    }
+
+    .sidebar {
+        position: sticky;
+        top: 0;
+        width: 200px;
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: #1f1f1f;
+    }
+
+    nav {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem;
+    }
+
+    nav a {
+        display: block;
+        text-transform: uppercase;
+        text-decoration: none;
+        color: #aaaaaa;
+        font-weight: 500;
+        transition: color .2s;
+    }
+
+    nav a:hover {
+        color: #ffffff;
+    }
+
+    main {
+        flex-grow: 1;
+        overflow: auto;
+    }
+</style>
