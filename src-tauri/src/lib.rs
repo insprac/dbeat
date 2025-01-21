3f9e5fdd609cfba42ebe0e7420bbca708d893d44
insprac <insprac.dev@gmail.com>
2025-01-22 08:23:27 +1300

    added open file location feature for Mac and Windows

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,10 +1,10 @@
-use std::{borrow::Cow, sync::Mutex};
+use std::{borrow::Cow, process::Command, sync::Mutex};
 
 use tauri::{Manager, State};
 
-mod settings;
 mod cue;
 mod fs_search;
+mod settings;
 
 struct AppState<'a> {
     recordings_dir: Option<Cow<'a, str>>,
@@ -12,11 +12,10 @@ struct AppState<'a> {
 
 impl<'a> Default for AppState<'a> {
     fn default() -> Self {
-        let recordings_dir = dirs::audio_dir()
-            .map(|mut dir| {
-                dir.push("PioneerDJ/Recording");
-                Cow::Owned(dir.to_string_lossy().into_owned())
-            });
+        let recordings_dir = dirs::audio_dir().map(|mut dir| {
+            dir.push("PioneerDJ/Recording");
+            Cow::Owned(dir.to_string_lossy().into_owned())
+        });
         Self { recordings_dir }
     }
 }
@@ -24,7 +23,6 @@ impl<'a> Default for AppState<'a> {
 #[tauri::command]
 async fn get_default_path() -> Option<String> {
     let dir = AppState::default().recordings_dir;
-    tracing::info!(?dir, "dir");
     dir.map(|s| s.to_string())
 }
 
@@ -37,13 +35,17 @@ async fn get_cue_sheet(path: &str) -> Result<cue::CueSheet, String> {
 }
 
 #[tauri::command]
-async fn find_cue_sheets(state: State<'_, Mutex<AppState<'_>>>) -> Result<Vec<cue::CueSheet>, String> {
+async fn find_cue_sheets(
+    state: State<'_, Mutex<AppState<'_>>>,
+) -> Result<Vec<cue::CueSheet>, String> {
     let state = state.lock().map_err(|err| {
         state.clear_poison();
         format!("app state lock was poisoned: {err}")
     })?;
 
-    let dir = state.recordings_dir.clone()
+    let dir = state
+        .recordings_dir
+        .clone()
         .ok_or_else(|| "recording directory not set".to_string())?;
 
     let instant = std::time::Instant::now();
@@ -52,6 +54,27 @@ async fn find_cue_sheets(state: State<'_, Mutex<AppState<'_>>>) -> Result<Vec<cu
     tracing::info!(?duration, found = sheets.len(), "searched cue sheets");
 
     Ok(sheets)
+}
+
+#[tauri::command]
+async fn open_file_location(path: &str) -> Result<(), String> {
+    #[cfg(target_os = "windows")]
+    {
+        Command::new("explorer")
+            .args(["/select,", &path])
+            .spawn()
+            .map_err(|e| e.to_string())?;
+    }
+
+    #[cfg(target_os = "macos")]
+    {
+        Command::new("open")
+            .args(["-R", &path])
+            .spawn()
+            .map_err(|e| e.to_string())?;
+    }
+
+    Ok(())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -66,7 +89,12 @@ pub fn run() {
             app.manage(Mutex::new(AppState::default()));
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![get_default_path, get_cue_sheet, find_cue_sheets])
+        .invoke_handler(tauri::generate_handler![
+            get_default_path,
+            get_cue_sheet,
+            find_cue_sheets,
+            open_file_location,
+        ])
         .run(tauri::generate_context!())
         .unwrap_or_else(|error| {
             tracing::error!(?error, "error while running tauri application");
